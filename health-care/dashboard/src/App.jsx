dd66280db29991aa5c1315aaa6d78a05ab1b596e
dharm16 <140909492+dharm16@users.noreply.github.com>
2024-07-23 16:34:56 +0530

    Update App.jsx

--- health-care/dashboard/src/App.jsx
+++ health-care/dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://healthcare-3tq6.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
